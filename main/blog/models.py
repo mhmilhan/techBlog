a05344560a62eaa941cc5726f12446b52ffb135c
unknown <mahmudulhasanmilhan@gmail.com>
2024-06-29 20:05:39 +0600

    feature post & special post basic backend done

--- main/blog/models.py
+++ main/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 class Category(models.Model):
@@ -41,8 +42,23 @@ class Post(models.Model):
     content = models.TextField(max_length=5000)
     status = models.CharField(max_length=10, choices=[('draft', 'Draft'), ('published', 'Published')], default='draft')
     is_featured = models.BooleanField(default=False)
+    is_latest = models.BooleanField(default=False)
+    is_popular = models.BooleanField(default=False)
+    is_trending = models.BooleanField(default=False)
+    is_unboxing = models.BooleanField(default=False)
+    is_review = models.BooleanField(default=False)
+    is_ai = models.BooleanField(default=False)
+    is_invention = models.BooleanField(default=False)
+    is_robotics = models.BooleanField(default=False)
+    is_space = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if self.status == 'published' and not self.published_at:
+            self.published_at = timezone.now()
+        super().save(*args, **kwargs)
--- main/main/views.py
+++ main/main/views.py
@@ -1,11 +1,34 @@
 from django.shortcuts import render
-from blog.models import Category
+from blog.models import Category, Post
 
 
 def index(request):
     categories = Category.objects.all()
+    posts = Post.objects.filter(status='published').order_by('-updated_at')
+    latest_posts = Post.objects.filter(is_latest=True).order_by('-updated_at')
+    featured_posts = Post.objects.filter(is_featured=True).order_by('-updated_at')
+    popular_posts = Post.objects.filter(is_popular=True).order_by('-updated_at')
+    trending_posts = Post.objects.filter(is_trending=True).order_by('-updated_at')
+    unboxing_posts = Post.objects.filter(is_unboxing=True).order_by('-updated_at')
+    reviews = Post.objects.filter(is_review=True).order_by('-updated_at')
+    ai_posts = Post.objects.filter(is_ai=True).order_by('-updated_at')
+    invention_posts = Post.objects.filter(is_invention=True).order_by('-updated_at')
+    robotics_posts = Post.objects.filter(is_robotics=True).order_by('-updated_at')
+    space_posts = Post.objects.filter(is_space=True).order_by('-updated_at')
+
     context = {
-        'categories': categories
+        'categories': categories,
+        'posts': posts,
+        'latest_posts': latest_posts,
+        'featured_posts': featured_posts,
+        'popular_posts': popular_posts,
+        'trending_posts': trending_posts,
+        'unboxing_posts': unboxing_posts,
+        'reviews': reviews,
+        'ai_posts': ai_posts,
+        'invention_posts': invention_posts,
+        'robotics_posts': robotics_posts,
+        'space_posts': space_posts,
     }
     return render(request, 'base.html', context)
 
